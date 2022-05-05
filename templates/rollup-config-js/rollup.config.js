03596ec48b7d8273332c5d7aab4cbf2614e6a538
neoddish <neoddish@outlook.com>
2022-05-05 20:32:08 +0800

    fix: rollup-config-js template

--- templates/rollup-config-js/rollup.config.js
+++ templates/rollup-config-js/rollup.config.js
@@ -1,17 +1,15 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
-module.exports = [
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.min.js",
-      name: "MyProject",
-      format: "umd",
-      sourcemap: false,
-    },
-    plugins: [resolve(), commonjs(), typescript(), terser()],
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.min.js',
+    name: 'MyProject',
+    format: 'umd',
+    sourcemap: false,
   },
-];
+  plugins: [resolve(), commonjs(), typescript(), terser()],
+};
